7b21dee2f2c9fc297fd203f3077f0fd9cb883147
Indopinetmart.user <indopinetmart8@gmail.com>
2026-03-06 18:05:00 +0700

    push

--- database/migrations/2026_03_06_160356_create_user_tokens_table.php
+++ database/migrations/2026_03_06_160356_create_user_tokens_table.php
@@ -6,62 +6,32 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('user_tokens', function (Blueprint $table) {
-
             $table->id();
 
             $table->foreignId('user_id')
                 ->constrained()
                 ->cascadeOnDelete();
 
-            // JWT Access Token
-            $table->text('token');
-
-            // Refresh token untuk generate token baru
+            $table->string('token', 512); // token VARCHAR agar bisa di-index
             $table->string('refresh_token', 100)->unique();
-
-            // device login (browser / mobile)
             $table->string('device', 255)->nullable();
-
-            // ip address user
             $table->string('ip_address', 45)->nullable();
-
-            // expired token
             $table->timestamp('expires_at')->nullable();
-
             $table->timestamps();
 
-            /*
-            |--------------------------------------------------------------------------
-            | INDEX OPTIMIZATION
-            |--------------------------------------------------------------------------
-            */
-
-            // cepat mencari token user
+            // INDEXES
             $table->index('user_id');
-
-            // validasi token cepat
             $table->index('token');
-
-            // refresh token lookup
             $table->index('refresh_token');
-
-            // cleanup token expired
             $table->index('expires_at');
 
-            // 1 device login enforcement
-            $table->unique(['user_id']);
+            $table->unique(['user_id']); // 1 device login enforcement
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('user_tokens');
