34bdd42491d84a885846fdb6a1635277ca172621
Indopinetmart.user <indopinetmart8@gmail.com>
2026-03-16 16:29:13 +0700

    Update .gitignore and modal edit view

--- resources/views/frontend/management/modal/modaleditdatawarga.blade.php
+++ resources/views/frontend/management/modal/modaleditdatawarga.blade.php
@@ -421,7 +421,7 @@
                                                         Batal
                                                     </button>
 
-                                                    <button type="submit" class="btn btn-primary btn-sm">
+                                                    <button type="submit" class="btn btn-warning btn-sm">
                                                         Kirim Pengajuan
                                                     </button>
 
