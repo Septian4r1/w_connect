8143ccb255ca57ea07adcba456b6f333041f2a53
Indopinetmart.user <indopinetmart8@gmail.com>
2026-03-16 17:14:37 +0700

    push_ok

--- resources/views/frontend/management/modal/modaleditdatawarga.blade.php
+++ resources/views/frontend/management/modal/modaleditdatawarga.blade.php
@@ -421,7 +421,7 @@
                                                         Batal
                                                     </button>
 
-                                                    <button type="submit" class="btn btn-purple btn-sm">
+                                                    <button type="submit" class="btn btn-primary btn-sm">
                                                         Kirim Pengajuan
                                                     </button>
 
